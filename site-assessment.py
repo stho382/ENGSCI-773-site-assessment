69071dc450fb2b6ab4fd46558a2f2abb0c9e425d
Sebastian Thomas <sebastianthomas588@gmail.com>
2023-03-16 14:45:54 +1300

    updated conda file

--- site-assessment.py
+++ site-assessment.py
@@ -386,8 +386,8 @@ def __main__():
     Runs all the plotting and data processing scripts
     '''
     
-    model_fs = 'GWA_file_name.lib'        # Note: must be a .lib file in the format downloaded from the GWA.
-    data_fs = 'CLIFLO_file_name.csv'      # Note: must be a .CSV file in the format from CLIFLO.
+    model_fs = 'gwa3_gwc_hvwz9a4m.lib'        # Note: must be a .lib file in the format downloaded from the GWA.
+    data_fs = 'station_list.csv'      # Note: must be a .CSV file in the format from CLIFLO.
 
     # load data and model results
     model = load_wind_atlas_file(model_fs)
